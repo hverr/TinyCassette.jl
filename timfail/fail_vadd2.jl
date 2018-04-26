4643ee0e45f778ed84f689dc441da798fe5d1d0e
Henri Verroken <henriverroken@gmail.com>
2018-04-26 15:53:13 +0200

    Make fail_vadd2 fail

--- timfail/fail_vadd2.jl
+++ timfail/fail_vadd2.jl
@@ -15,6 +15,8 @@ end
 
 Base.size(g::CuDeviceArray) = g.shape
 
+Base.checkbounds(::CuDeviceArray, I...) = nothing
+
 for dim in (:x, :y, :z)
     # Thread index
     fn = Symbol("threadIdx_$dim")
@@ -49,15 +51,17 @@ function kernel_vadd(a, b, c)
 end
 
 
-code_llvm(STDOUT, kernel_vadd, Tuple{CuDeviceArray{Float32,1},
+code_llvm(stdout, kernel_vadd, Tuple{CuDeviceArray{Float32,1},
                                      CuDeviceArray{Float32,1},
                                      CuDeviceArray{Float32,1}})
 
 
-using Cassette
-Cassette.@context Ctx
+using TinyCassette
+struct Ctx end
 
-code_llvm(STDOUT, Cassette.overdub(Ctx, kernel_vadd),
-          Tuple{CuDeviceArray{Float32,1},
+code_llvm(stdout, TinyCassette.execute,
+          Tuple{Ctx,
+                typeof(kernel_vadd),
+                CuDeviceArray{Float32,1},
                 CuDeviceArray{Float32,1},
                 CuDeviceArray{Float32,1}})
