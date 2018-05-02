519e981dbe41c345f90bab432ee65397b0cf62c8
Henri Verroken <henriverroken@gmail.com>
2018-05-02 14:13:40 +0200

    Use named tuples in tests

--- timfail/fail_vadd2_1_1.jl
+++ timfail/fail_vadd2_1_1.jl
@@ -17,34 +17,24 @@ Base.size(g::CuDeviceArray) = g.shape
 
 Base.checkbounds(::CuDeviceArray, I...) = nothing
 
-for dim in (:x, :y, :z)
-    # Thread index
-    fn = Symbol("threadIdx_$dim")
-    @eval @inline $fn() = Int((ccall($"llvm.nvvm.read.ptx.sreg.tid.$dim", llvmcall, UInt32, ()))+UInt32(1))
+@inline blockIdx_x() = Int((ccall("llvm.nvvm.read.ptx.sreg.ctaid.x", llvmcall, UInt32, ()))+UInt32(1))
+@inline blockIdx_y() = Int((ccall("llvm.nvvm.read.ptx.sreg.ctaid.y", llvmcall, UInt32, ()))+UInt32(1))
+@inline blockIdx_z() = Int((ccall("llvm.nvvm.read.ptx.sreg.ctaid.z", llvmcall, UInt32, ()))+UInt32(1))
 
-    # Block size (#threads per block)
-    fn = Symbol("blockDim_$dim")
-    @eval @inline $fn() =  Int(ccall($"llvm.nvvm.read.ptx.sreg.ntid.$dim", llvmcall, UInt32, ()))
+@inline blockDim_x() = Int((ccall("llvm.nvvm.read.ptx.sreg.ntid.x", llvmcall, UInt32, ()))+UInt32(1))
+@inline blockDim_y() = Int((ccall("llvm.nvvm.read.ptx.sreg.ntid.y", llvmcall, UInt32, ()))+UInt32(1))
+@inline blockDim_z() = Int((ccall("llvm.nvvm.read.ptx.sreg.ntid.z", llvmcall, UInt32, ()))+UInt32(1))
 
-    # Block index
-    fn = Symbol("blockIdx_$dim")
-    @eval @inline $fn() = Int((ccall($"llvm.nvvm.read.ptx.sreg.ctaid.$dim", llvmcall, UInt32, ()))+UInt32(1))
+@inline threadIdx_x() = Int((ccall("llvm.nvvm.read.ptx.sreg.tid.x", llvmcall, UInt32, ()))+UInt32(1))
+@inline threadIdx_y() = Int((ccall("llvm.nvvm.read.ptx.sreg.tid.y", llvmcall, UInt32, ()))+UInt32(1))
+@inline threadIdx_z() = Int((ccall("llvm.nvvm.read.ptx.sreg.tid.z", llvmcall, UInt32, ()))+UInt32(1))
 
-    # Grid size (#blocks per grid)
-    fn = Symbol("gridDim_$dim")
-    @eval @inline $fn() =  Int(ccall($"llvm.nvvm.read.ptx.sreg.nctaid.$dim", llvmcall, UInt32, ()))
-end
-
-@inline gridDim() =   (x=gridDim_x(),   y=gridDim_y(),   z=gridDim_z())
-
-@inline blockIdx() =  (x=blockIdx_x(),  y=blockIdx_y(),  z=blockIdx_z())
-
-@inline blockDim() =  (x=blockDim_x(),  y=blockDim_y(),  z=blockDim_z())
-
+@inline blockIdx() = (blockIdx_x(), blockIdx_y(), blockIdx_z())
+@inline blockDim() = (blockDim_x(), blockDim_y(), blockDim_z())
 @inline threadIdx() = (x=threadIdx_x(), y=threadIdx_y(), z=threadIdx_z())
 
 function kernel_vadd(a, b, c)
-    i = (blockIdx().x-1) * blockDim().x + threadIdx().x
+    i = threadIdx().x
     c[i] = a[i] + b[i]
 
     return nothing
